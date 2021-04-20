5756b66cb0ff04e4a1268cfdbb56707a97df6ae9
Zamir Lalji <47802606+Zamir95@users.noreply.github.com>
2021-04-19 22:17:00 -0400

    Add files via upload

--- mort.c
+++ mort.c
@@ -3,40 +3,32 @@
 
 int main() {
 
-	float value, rate;
-	int years;
+    float amount, rate;
+    int years;
 
-	printf("The initial mortgage value: \n");
-	scanf("%f", &value);
+    printf("The initial mortgage value: \n");
+    scanf("%f", &amount);
 
-	printf("The mortgage rate: \n" );
-	scanf("%f", &rate);
+    printf("The mortgage rate: \n" );
+    scanf("%f", &rate);
 
-	printf("How many years: \n");
-	scanf("%d", &years);
-	
-	printf("For a mortgage of %.2f for %d years and interest rate of %.2f%% \n", value, years, rate);
+    printf("How many years: \n");
+    scanf("%d", &years);
 
-	float monthly_rate = (rate/100)/12;
-	int x = years * 12;
-	float formula = pow(1 + monthly_rate, x);
-	float p = value * (monthly_rate * formula) / (formula - 1);
-	
-	float intrate = value*monthly_rate;
-	
-	int i;
-	for ( i = 1; i <= x ; ++i) {
+    printf("For a mortgage of %.2f for %d years and interest rate of %.2f \n", amount, years, rate);
 
-		value = value + intrate - p;
-		intrate = value * monthly_rate;
+    float monthlyRate = (rate / 100) / 12;
+    int x = years * 12;
+    float fl = pow(1 + monthlyRate, x);
+    float princ = amount * (monthlyRate * fl) / (fl - 1);
+    float interestRate = amount * monthlyRate;
 
-		printf("%*d\t\t%.2f \n",3, i, value);
-	}
-	
-	return 0;
+    int i;
+    for (i = 1; i <= x; ++i) {
+        amount = amount + interestRate - princ;
+        interestRate = amount * monthlyRate;
 
-
-
+        printf("%3d        %.2f \n", i, amount);
+    }
+    return 0;
 }
-
- 
